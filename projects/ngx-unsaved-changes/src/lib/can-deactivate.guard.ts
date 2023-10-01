be999460221606ba0e3e99c72fc0cb51cf01e778
Zenith Wogwugwu <uzenith360@gmail.com>
2023-10-01 18:25:38 +0100

    feat: upgrade to angular 16

--- projects/ngx-unsaved-changes/src/lib/can-deactivate.guard.ts
+++ projects/ngx-unsaved-changes/src/lib/can-deactivate.guard.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@angular/core';
-import { CanDeactivate } from '@angular/router';
+
 import { ComponentCanDeactivate } from './component-can-deactivate';
 import EnvironmentConfig from './environment-config.interface';
 import EnvironmentConfigService from './environment-config.service';
@@ -7,7 +7,7 @@ import EnvironmentConfigService from './environment-config.service';
 @Injectable({
   providedIn: 'root'
 })
-export class CanDeactivateGuard implements CanDeactivate<ComponentCanDeactivate> {
+export class CanDeactivateGuard  {
   constructor(@Inject(EnvironmentConfigService) private config: EnvironmentConfig){}
 
   canDeactivate(component: ComponentCanDeactivate): boolean {
