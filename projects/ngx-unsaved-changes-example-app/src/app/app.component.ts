f2dd68486bd2a3d7767de554c588b9c4896146c4
Zenith Wogwugwu <uzenith360@gmail.com>
2025-04-01 14:44:52 +0100

    fix: migrations

--- projects/ngx-unsaved-changes-example-app/src/app/app.component.ts
+++ projects/ngx-unsaved-changes-example-app/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
+
 import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet]
+    imports: [RouterOutlet]
 })
 export class AppComponent {
   title = 'ngx-unsaved-changes-example-app';
